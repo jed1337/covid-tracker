84e90349920c875260f3e36577cfc0a374d37ee3
jed1337 <j.caychingco@yahoo.com>
2020-05-16 11:27:45 +0800

    Add Charts

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,19 @@ import axios from 'axios'
 
 const url = 'https://covid19.mathdro.id/api'
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
+
     try {
-        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableUrl);
 
         return {confirmed, recovered, deaths, lastUpdate}
     } catch (e) {
-
+        console.log('Error: ', e)
     }
 }
 
@@ -23,6 +29,16 @@ export const fetchDailyData = async () => {
             }))
         return modifiedData
     } catch (e) {
+        console.log('Error: ', e)
+    }
+}
 
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`)
+
+        return countries.map(country => country.name)
+    } catch (e) {
+        console.log('Error: ', e)
     }
 }
